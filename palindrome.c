c70514413230845f01435de7dbfcbb7eed5c41b0
Shashwatw7 <75832595+Shashwatw7@users.noreply.github.com>
2020-12-18 01:23:57 +0530

    Add files via upload

---
+++ palindrome.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<math.h>
+int reverse (int num);
+int isPalindrome(int num);
+int main()
+{
+	int num;
+	printf("enter no: ");
+	scanf("%d",&num);
+	
+	if(isPalindrome(num)==1)
+	{
+		printf("%d is palindr no.\n",num);
+	}
+	else
+	{
+		printf("%d is not,\n",num);
+	}
+	return 0;
+}
+int isPalindrome(int num)
+{
+	if(num== reverse(num))
+	{
+		return 1;
+	}
+	return 0;
+}
+
+int reverse(int num)
+{
+int digit=(int)log10(num);
+if(num==0)
+return 0;
+
+return((num%10*pow(10,digit))+reverse(num/10));
+}
